2422040d109eea14c1cc581595e3c6412d114adc
takahashi-ei <favoritpastview12358@yahoo.co.jp>
2013-08-23 18:11:34 +0900

    eiichi test

---
+++ MyMovie/MovieDB.h
@@ -0,0 +1,18 @@
+//
+//  MovieDB.h
+//  MyMovie
+//
+//  Created by 英一 高橋 on 2013/08/23.
+//  Copyright (c) 2013年 fett. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface MovieDB : NSManagedObject
+
+@property (nonatomic, retain) UNKNOWN_TYPE evaluation;
+@property (nonatomic, retain) UNKNOWN_TYPE attribute;
+
+@end
---
+++ MyMovie/MovieDB.m
@@ -0,0 +1,17 @@
+//
+//  MovieDB.m
+//  MyMovie
+//
+//  Created by 英一 高橋 on 2013/08/23.
+//  Copyright (c) 2013年 fett. All rights reserved.
+//
+
+#import "MovieDB.h"
+
+
+@implementation MovieDB
+
+@dynamic evaluation;
+@dynamic attribute;
+
+@end
